65ee9f8768a2b5414ed9847be01d949423b9b509
rawfalafel <yutaro.mori@10gen.com>
2013-09-18 14:27:56 -0400

    Add angular HTML elements and initial Controller logic for angular.js

--- static/js/gol-angular.js
+++ static/js/gol-angular.js
@@ -1,1 +1,15 @@
-var a = 1;
+function Grid($scope) {
+    var grid = [], gridSize = 10;
+    for (var i = 0; i < gridSize; i++) {
+        grid[i] = [];
+        for (var j = 0; j < gridSize; j++) {
+            grid[i][j] = 0;
+        }
+    }
+
+    $scope.grid = grid;
+
+    $scope.toggle = function(x, y) {
+        $scope.grid[y][x] = ($scope.grid[y][x] == 1) ? 0 : 1;
+    }
+}
