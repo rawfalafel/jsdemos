399e376d3e8d452471f7c0799889d147cf770d23
rawfalafel <yutaro.mori@10gen.com>
2013-10-08 16:10:10 -0400

    Fix select2 directive to be compatible with form validation.

--- static/js/forms.js
+++ static/js/forms.js
@@ -3,23 +3,21 @@
 
     var myApp = angular.module('myApp', []);
 
-    myApp.directive('select2', function($parse) {
-        return function(scope, element, attrs) {
-            $(function() {
-                var $el = $(element).select2();
-                scope.$watch(attrs.select2, function(v) {
-                    $el.select2('val', v);
+    myApp.directive('select2', function() {
+        return {
+            require: 'ngModel',
+            link: function(scope, element, attrs) {
+                $(function() {
+                    var $el = $(element).select2();
+                    scope.$watch(attrs.ngModel, function(v) {
+                        $el.select2('val', v);
+                    });
                 });
-                $el.on('change', function(e) {
-                    $parse(attrs.select2).assign(scope, e.val);
-                    scope.$apply();
-                });
-            });
+            }
         };
     });
 
     myApp.controller('formController', function($scope) {
-
         $scope.$watch('girls + drinks', function(newValue, oldValue) {
             if ($scope.selectForm.$valid) {
                 console.log("save that shit");
